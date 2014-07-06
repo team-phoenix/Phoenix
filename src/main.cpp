7204574082be6e12d13ca783810592098c79eab3
Gu1 <gu1@aeroxteam.fr>
2014-07-06 13:56:45 +0200

    Fix unhandled SIGIOs on ALSA/Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
 #include <QtQuick/QQuickView>
 
+#ifdef Q_OS_LINUX
+#include <pthread.h>
+#include <signal.h>
+#endif
+
 #include "video-gl.h"
 #include "core.h"
 
 int main( int argc, char *argv[] ) {
+#ifdef Q_OS_LINUX
+    // When using QAudioOutput on linux/ALSA, we need to
+    // block SIGIO on all threads except the audio thread
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGIO);
+    pthread_sigmask(SIG_BLOCK, &set, nullptr);
+#endif
     QGuiApplication a( argc, argv );
     
     qmlRegisterType<GLWindow>("GLWindow", 1, 0, "GLWindow");
