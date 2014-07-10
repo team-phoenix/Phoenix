7af0963d0d4417c4d0c461ab56ef7588a25eac7f
Gu1 <gu1@aeroxteam.fr>
2014-07-10 21:42:15 +0200

    Follow Qt's coding style (remove spaces around and inside parenthesis)

--- include/core.h
+++ include/core.h
@@ -26,39 +26,39 @@ struct LibretroSymbols {
     LibretroSymbols();
     
     // Libretro core functions
-    unsigned( *retro_api_version )( void );
-    void ( *retro_cheat_reset )( void );
-    void ( *retro_cheat_set )( unsigned , bool , const char * );
-    void ( *retro_deinit )( void );
-    void *( *retro_get_memory_data )( unsigned );
-    size_t ( *retro_get_memory_size )( unsigned );
-    unsigned( *retro_get_region )( void );
-    void ( *retro_get_system_av_info )( struct retro_system_av_info * );
-    void ( *retro_get_system_info )( struct retro_system_info * );
-    void ( *retro_init )( void );
-    bool ( *retro_load_game )( const struct retro_game_info * );
-    bool ( *retro_load_game_special )( unsigned , const struct retro_game_info *, size_t );
-    void ( *retro_reset )( void );
-    void ( *retro_run )( void );
-    bool ( *retro_serialize )( void *, size_t );
-    size_t ( *retro_serialize_size )( void );
-    void ( *retro_unload_game )( void );
-    bool ( *retro_unserialize )( const void *, size_t );
+    unsigned (*retro_api_version)(void);
+    void (*retro_cheat_reset)(void);
+    void (*retro_cheat_set)(unsigned , bool , const char *);
+    void (*retro_deinit)(void);
+    void *(*retro_get_memory_data)(unsigned);
+    size_t (*retro_get_memory_size)(unsigned);
+    unsigned (*retro_get_region)(void);
+    void (*retro_get_system_av_info)(struct retro_system_av_info *);
+    void (*retro_get_system_info)(struct retro_system_info *);
+    void (*retro_init)(void);
+    bool (*retro_load_game)(const struct retro_game_info *);
+    bool (*retro_load_game_special)(unsigned , const struct retro_game_info *, size_t);
+    void (*retro_reset)(void);
+    void (*retro_run)(void);
+    bool (*retro_serialize)(void *, size_t);
+    size_t (*retro_serialize_size)(void);
+    void (*retro_unload_game)(void);
+    bool (*retro_unserialize)(const void *, size_t);
     
     // Frontend-defined callbacks
-    void ( *retro_set_audio_sample )( retro_audio_sample_t );
-    void ( *retro_set_audio_sample_batch )( retro_audio_sample_batch_t );
-    void ( *retro_set_controller_port_device )( unsigned, unsigned );
-    void ( *retro_set_environment )( retro_environment_t );
-    void ( *retro_set_input_poll )( retro_input_poll_t );
-    void ( *retro_set_input_state )( retro_input_state_t );
-    void ( *retro_set_video_refresh )( retro_video_refresh_t );
+    void (*retro_set_audio_sample)(retro_audio_sample_t);
+    void (*retro_set_audio_sample_batch)(retro_audio_sample_batch_t);
+    void (*retro_set_controller_port_device)(unsigned, unsigned);
+    void (*retro_set_environment)(retro_environment_t);
+    void (*retro_set_input_poll)(retro_input_poll_t);
+    void (*retro_set_input_state)(retro_input_state_t);
+    void (*retro_set_video_refresh)(retro_video_refresh_t);
     
     // Optional core-defined callbacks
-    void ( *retro_audio )();
-    void ( *retro_audio_set_state )( bool enabled );
-    void ( *retro_frame_time )( retro_usec_t delta );
-    void ( *retro_keyboard_event )( bool down, unsigned keycode, uint32_t character, uint16_t key_modifiers );
+    void (*retro_audio)();
+    void (*retro_audio_set_state)(bool enabled);
+    void (*retro_frame_time)(retro_usec_t delta);
+    void (*retro_keyboard_event)(bool down, unsigned keycode, uint32_t character, uint16_t key_modifiers);
     
 };
 
@@ -108,8 +108,8 @@ public:
     void setInputStateCallBack(unsigned port, unsigned device, unsigned index, unsigned id);
 
     // Initilization methods
-    bool loadCore( const char * path );
-    bool loadGame( const char * path );
+    bool loadCore(const char * path);
+    bool loadGame(const char * path);
 
     // Unload
     void unload();
@@ -154,13 +154,13 @@ private:
     QMap<unsigned, bool> joypad;
 
     // Callbacks
-    static void audioSampleCallback( int16_t left, int16_t right );
-    static size_t audioSampleBatchCallback( const int16_t *data, size_t frames );
-    static bool environmentCallback( unsigned cmd, void *data );
-    static void inputPollCallback( void );
-    static void logCallback( enum retro_log_level level, const char *fmt, ... );
-    static int16_t inputStateCallback( unsigned port, unsigned device, unsigned index, unsigned id );
-    static void videoRefreshCallback( const void *data, unsigned width, unsigned height, size_t pitch );
+    static void audioSampleCallback(int16_t left, int16_t right);
+    static size_t audioSampleBatchCallback(const int16_t *data, size_t frames);
+    static bool environmentCallback(unsigned cmd, void *data);
+    static void inputPollCallback(void);
+    static void logCallback(enum retro_log_level level, const char *fmt, ...);
+    static int16_t inputStateCallback(unsigned port, unsigned device, unsigned index, unsigned id);
+    static void videoRefreshCallback(const void *data, unsigned width, unsigned height, size_t pitch);
     
 };
 
