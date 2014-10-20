a3c1d5a940c8f6b918a7a2f62a3b425a7f476dfc
Druage <Druage@gmx.com>
2014-10-19 22:06:56 -0400

    Removed unused InputDevice signal.

--- include/inputdevice.h
+++ include/inputdevice.h
@@ -31,7 +31,6 @@ public:
 
 signals:
     void inputEventReceived(InputDeviceEvent *ev, int16_t value);
-    void inputEventReceivedQML(QString name, int16_t value);
 
 public slots:
     QString deviceName() const { return device_name; }
