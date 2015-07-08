73829dd6250bec10c0f8e331e8c3cc849237698b
Lee <Druage@gmx.com>
2015-07-09 01:27:29 +0300

    Remove all of the source code...

--- src/logging.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "logging.h"
-
-
-Q_LOGGING_CATEGORY( phxCore, "phoenix.core" )
-Q_LOGGING_CATEGORY( phxVideo, "phoenix.video" )
-Q_LOGGING_CATEGORY( phxAudio, "phoenix.audio" )
-Q_LOGGING_CATEGORY( phxInput, "phoenix.input" )
-Q_LOGGING_CATEGORY( phxLibrary, "phoenix.library" )
