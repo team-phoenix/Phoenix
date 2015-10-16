2141e5e783b44cc8c3097dbe84a0d8b639c085af
athairus <athairus@gmail.com>
2015-10-15 22:07:35 -0400

    Portable mode: Fixed wrong path for cores

--- frontend/cpp/library/phxpaths.cpp
+++ frontend/cpp/library/phxpaths.cpp
@@ -100,7 +100,7 @@ void PhxPaths::createAllPaths() {
     if( mPortableMode ) {
         qCDebug( phxLibrary ) << "Portable mode";
         PhxPaths::mResourceLocation = PhxPaths::mBinLocation;
-        PhxPaths::mCoreLocation = PhxPaths::mUserDataLocation % '/' % QStringLiteral( "Cores" );
+        PhxPaths::mCoreLocation = PhxPaths::mBinLocation % '/' % QStringLiteral( "Cores" );
         PhxPaths::mMetadataLocation = PhxPaths::mBinLocation % '/' % QStringLiteral( "Metadata" ) % '/';
 
         PhxPaths::mUserDataLocation = PhxPaths::mBinLocation % '/' % QStringLiteral( "User Data" );
