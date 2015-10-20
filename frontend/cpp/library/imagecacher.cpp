447751625a03a540c4cc882460aaff4590602109
athairus <athairus@gmail.com>
2015-10-20 00:30:03 -0400

    Stop printing cached image filename

--- frontend/cpp/library/imagecacher.cpp
+++ frontend/cpp/library/imagecacher.cpp
@@ -21,8 +21,6 @@ void ImageCacher::cache() {
 
     QString cachedFile = PhxPaths::coverArtCacheLocation() % '/' % identifier() % '.' % mImageType;
 
-    qDebug() << cachedFile;
-
     if( !QFile::exists( cachedFile ) && imageUrl().isValid() ) {
 
         QNetworkRequest request( imageUrl() );
