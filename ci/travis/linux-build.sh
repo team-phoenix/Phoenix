aa6c3c98ce03db2e873e5b39f24e208daf895d38
athairus <athairus@gmail.com>
2017-06-04 00:05:47 -0700

    Added missing folders

--- ci/travis/linux-build.sh
+++ ci/travis/linux-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir "build" "install"
 cmake .. -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_INSTALL_PREFIX="${PWD}/install"
 cmake --build ./build --target "build"
 cmake --build ./build --target "install"
