3a10732e839f5982602b00bc0d15c169cd9f44cc
athairus <athairus@gmail.com>
2017-06-10 15:08:33 -0700

    WIP

--- ci/travis/linux-build.sh
+++ ci/travis/linux-build.sh
@@ -3,8 +3,9 @@
 set -e # exit on error
 set -x # echo commands
 
+cd ..
 mkdir "build" "install"
 cd build
-cmake -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" -DCMAKE_INSTALL_PREFIX="${PWD}/../install" ..
+cmake -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" -DCMAKE_INSTALL_PREFIX="${PWD}/../install" ./Phoenix
 cmake --build .
 cmake --build . --target "install"
