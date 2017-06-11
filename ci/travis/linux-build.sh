63df9376989eab9c8911e608757365113c2b79f8
athairus <athairus@gmail.com>
2017-06-10 17:10:17 -0700

    WIP

--- ci/travis/linux-build.sh
+++ ci/travis/linux-build.sh
@@ -5,7 +5,7 @@ set -x # echo commands
 
 mkdir "build" "install"
 cd build
-cmake -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" -DCMAKE_INSTALL_PREFIX="${PWD}/../install" ..
+cmake -DCMAKE_CXX_COMPILER_LAUNCHER="ccache" -DCMAKE_INSTALL_PREFIX="$PWD/../install" ..
 cat version.h
 cmake --build . --target "all"
 cmake --build . --target "install"
