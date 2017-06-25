681ce4aa8aacb902a1b8aee9121d19d14884faf0
athairus <athairus@gmail.com>
2017-06-25 12:55:18 -0700

    Windows: Call windeployqt on install

--- CMake/DistWindows.cmake
+++ CMake/DistWindows.cmake
@@ -7,3 +7,17 @@ install( DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/Metadata DESTINATION . )
 if( PORTABLE_MODE )
 	install( FILES ${CMAKE_CURRENT_BINARY_DIR}/PHOENIX-PORTABLE DESTINATION . )
 endif()
+
+# This tool automatically gathers Qt dependencies and copies them to the install folder
+set(QT5_WINDEPLOYQT ${Qt5_DIR}/../../../bin/windeployqt)
+#get_filename_component(SDL2_DIR_PARENT "${SDL2_DIR}" DIRECTORY)
+#--pdb
+install(CODE "
+        message(\"Executing windeployqt...\")
+        execute_process(COMMAND \"${QT5_WINDEPLOYQT}\"
+            --compiler-runtime
+            --qmldir \"${CMAKE_INSTALL_PREFIX}/QML\"
+            \"${CMAKE_INSTALL_PREFIX}/Phoenix.exe\"
+        )
+        message(\"windeployqt finished!\")
+        ")
