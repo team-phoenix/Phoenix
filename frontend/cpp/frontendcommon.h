e23164309633b35b7213e7043e31bd1b2ff7da09
athairus <athairus@gmail.com>
2017-07-21 15:26:53 +0900

    Include Qt class headers instead of Qt module headers to speed up compilation

--- frontend/cpp/frontendcommon.h
+++ frontend/cpp/frontendcommon.h
@@ -1,25 +1,56 @@
-#ifndef FRONTENDCOMMON_H
-#define FRONTENDCOMMON_H
+#pragma once
 
 #ifdef __cplusplus
 
-#include <QtCore>
-#include <QtWidgets>
-#include <QtConcurrent>
-#include <QtQml>
-#include <QtQuick>
-#include <QtSql>
-#include <QtMultimedia>
-#include <QtNetwork>
-
 #include <memory>
 
-#include "quazip.h"
-#include "JlCompress.h"
-#include "zlib.h"
+#include <QtGlobal>
+
+//#include <QtCore>
+#include <QString>
+#include <QStringBuilder>
+#include <QThread>
+#include <QStandardPaths>
+#include <QAbstractTableModel>
+#include <QDir>
+#include <QDirIterator>
+#include <QtMath>
+
+//#include <QtConcurrent>
+#include <QtConcurrentMap>
+
+//#include <QtGui>
+#include <QTextDocument>
+#include <QGuiApplication>
+
+//#include <QtMultimedia>
+
+//#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+//#include <QtQml>
+#include <QQmlEngine>
+#include <QJSEngine>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+//#include <QtQuick>
+
+//#include <QtSql>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+#include <QSqlError>
+#include <QSqlDriver>
+#include <QSqlField>
+
+//#include <QtWidgets>
+#include <QApplication>
+
+#include <quazip.h>
+#include <JlCompress.h>
+#include <zlib.h>
+
 #include "logging.h"
 
 #endif
-
-#endif // FRONTENDCOMMON_H
-
